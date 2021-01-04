ffcbb101c0d5b6375541d323e0d6bcbe157b0b79
Michael Lastric <michaell2915@5565.ca>
2021-01-04 20:06:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("This Shopping Assistant was made and designed by Michael L. and Ethan C. This is currently in beta, enjoy!")
+
+
+print("\nSHOPPING ASSISTANT V1.0\n\nPLEASE PRESS A BUTTON TO BEGIN.")
+
+
+
